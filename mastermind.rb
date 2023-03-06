68298d5e32779c058d2284210f9b76e8e46a0fd4
kyungminkim1 <kyungminkimnz@gmail.com>
2023-03-06 21:42:04 +1300

    Begin pseudocode

---
+++ mastermind.rb
@@ -0,0 +1,28 @@
+=begin
+
+Recreate a game of Mastermind using Ruby
+
+Rough outline of code architecture below
+
+class Player
+
+end
+
+class Computer
+
+end
+
+class Code
+  @code
+
+end
+
+class Board
+
+end
+
+class Game (manages game state)
+
+end
+
+=end
